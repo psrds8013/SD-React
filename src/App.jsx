5b52cbdcf9a10a1e152e26060d08b9d31e8c6d39
pardeep <mahesh@shoutaboutus.com>
2025-03-20 17:37:01 +0530

    update banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Component/Header';
 import Footer from './Component/Footer';
 import './App.css';
+import BannerImage from "./assets/img/banner.jpg";
 import FeaturedShows from "./assets/img/ticket-icon.svg";
 import FeaturedImage1 from "./assets/img/the-last-laugh-image-1730348173-870.jpg";
 import FeaturedImage2 from "./assets/img/come-alive-image-1721213649-826.jpg";
@@ -15,21 +16,12 @@ function App() {
     <>
       <Header/>
       <main className="main">
-        <section id="hero" className="hero section dark-background">
-          <div className="container">
-            <div className="row gy-4">
-              <div className="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center" data-aos="fade-in">
-                <h1>Your new digital experience with Bocor</h1>
-                <p>We are team of talented designers making websites with Bootstrap</p>
-                <div className="d-flex">
-                  <a href="#about" className="btn-get-started">Get Started</a>
-                </div>
-              </div>
-              <div className="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-out" data-aos-delay="100">
-                <img src="assets/img/hero-img.png" className="img-fluid animated" alt=""/>
+        <section id="hero" className="hero section relative">
+            <div className="row align-items-center">
+              <div className="col-12">
+                <img src={BannerImage} className="img-fluid w-100" alt="Banner" />
               </div>
             </div>
-          </div>
         </section>
         <section id="features" className="features section">
           <div className="container section-title" data-aos="fade-up">
